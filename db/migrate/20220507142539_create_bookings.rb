1256f1c7fffa75447012d79a7ce4867b05d9e345
Andrew Morton <ammorton22@gmail.com>
2022-05-07 15:26:42 +0100

    Bike model created

---
+++ db/migrate/20220507142539_create_bookings.rb
@@ -0,0 +1,13 @@
+class CreateBookings < ActiveRecord::Migration[6.1]
+  def change
+    create_table :bookings do |t|
+      t.float :price
+      t.date :start_date
+      t.date :end_date
+      t.references :user, null: false, foreign_key: true
+      t.references :bike, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
