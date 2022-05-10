f0c0698f5913dd4aa2db0316b36d70ef8b1aa0f5
Andrew Morton <ammorton22@gmail.com>
2022-05-10 21:17:31 +0100

    Created a bookings controller and first attempt at a bookings form

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,20 @@
 class BookingsController < ApplicationController
+def new
+  @bike = Bike.find(params[:bike_id])
+  @booking = Booking.new
+end
+
+def create
+  @booking = Booking.new(booking_params)
+  @bike = Bike.find(params[:bike_id])
+  @booking.bike = @bike
+  @booking.save
+  redirect_to bike_path(@bike)
+end
+
+private
+
+def booking_params
+  params.require(:booking).permit(:start_date, :end_date)
+end
 end
