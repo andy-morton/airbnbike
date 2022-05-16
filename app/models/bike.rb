b0a69222fadb1b505646f33f1bb31097683422ea
Josh Ackroyd <ackroydjosh93@gmail.com>
2022-05-16 18:35:54 +0100

    added categories and size to bike model

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -2,11 +2,9 @@ class Bike < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_many_attached :photos
-  validates :name, presence: true
-  validates :location, presence: true
+  validates :name, :location, :size, :category, :price, presence: true
   validates :name, uniqueness: { scope: :user,
                                  message: "All your bikes should have a unique name" }
-  validates :price, presence: true
-  validates :description, length: { minimum: 20,
+  validates :description, length: { minimum: 10,
                                     message: "Please add more detail to your bike description" }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_14_100820) do
+ActiveRecord::Schema.define(version: 2022_05_15_161200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,8 @@ ActiveRecord::Schema.define(version: 2022_05_14_100820) do
     t.datetime "updated_at", precision: 6, null: false
     t.float "latitude"
     t.float "longitude"
+    t.string "category"
+    t.string "size"
     t.index ["user_id"], name: "index_bikes_on_user_id"
   end
 
